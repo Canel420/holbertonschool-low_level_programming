2edfb7ab076b786c108bc6cf4467ff0b4510f226
Canel420 <4521@holbertonschool.com>
2022-02-11 15:54:50 -0500

    Prints the alphabet in lowercase except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,8 @@ int main(void)
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		putchar(ch);
-		if (ch == 'e' && ch == 'q')
-		{
-			putchar('');
-		}
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
 	}
 
 	putchar('\n');
