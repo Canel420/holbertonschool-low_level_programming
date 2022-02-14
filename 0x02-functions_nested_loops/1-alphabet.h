e4ec31f6800b86ead71db338656c1d937b6a8d91
Canel420 <4521@holbertonschool.com>
2022-02-14 15:31:39 -0500

    function that prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "_putchar.h"
+
+void print_alphabet(void);
+
+/**
+ * print_alphabet - Alphabet in lowercases.
+ *
+ * Description: this function prints the alphabet in lowercases.
+ * Section header : uses holberton _putchar function.
+ * Return : Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
