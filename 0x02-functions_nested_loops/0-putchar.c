d856c1f6ea75977b110e2b5fbdbf230a5b4ddbeb
Canel420 <4521@holbertonschool.com>
2022-02-15 20:30:08 -0500

    Reuploading file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints the word _putchar
+ *
+ * Description: prints the word _putchar using the function
+ * _putchar.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ltrs[8] = "_putchar";
+	int i;
+
+	for (i = 0 ; i <= 8 ; i++)
+		_putchar(ltrs[i]);
+	_putchar('\n');
+
+	return (0);
+}
