51e9fa2f25e4d0ece3ea9e85d4d60dff0ce8959b
Canel420 <4521@holbertonschool.com>
2022-02-17 14:31:31 -0500

    function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = (a * b)
+
+	if (a > 9 && a < 99)
+		a = _putchar(((a / 10) + '0') && ((a % 10) + '0'));
+	else if (a > 99 && a < 999)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-
+void print_numbers(void);
+void print_most_numbers(void);
 
 
 
