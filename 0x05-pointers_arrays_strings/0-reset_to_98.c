446957690dd84d16724b5c8493de7a1a67642e63
Canel420 <4521@holbertonschool.com>
2022-02-21 13:59:10 -0500

    First pointer use

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - Function that updates the value it points to 98.
  *
- * @*n: Pointer to an integer.
+ * @n: Pointer to an integer.
  *
  * Description: Takes a pointer to an int as parameter and updates
  * the value it points to to 98.
