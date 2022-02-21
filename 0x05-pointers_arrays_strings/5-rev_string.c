04549af93e3dc9c25e2c9db7563f8535b819769b
Canel420 <4521@holbertonschool.com>
2022-02-21 18:59:31 -0500

    Reverse string array

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string -  Function that prints a string, in reverse
+ * @s: Pointer to the characters of a string.
+ *
+ * Description: Takes an array of characters and reverse
+ * each character.
+ *
+ * Return: The reversed string.
+ */
+
+void rev_string(char *s)
+{
+	int len2 = 0 , len1 = 0 ;
+	char ch;
+
+	while (s[len1] != '\0')
+	{
+		len1++;
+	}
+
+	len1 = len1 -1;
+	while(len2 <= len1)
+	{
+		ch = s[len2];
+		s[len2] = s[len1];
+		s[len1] = ch;
+
+		len2++;
+		len1--;
+	}
+}
