869d839199a65400de7c1a38693dfd1a62b22d1e
Canel420 <4521@holbertonschool.com>
2022-02-21 20:10:10 -0500

    Half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,10 +20,10 @@ void puts_half(char *str)
 	}
 
 	if (len % 2 == 0)
-		for (i = len / 2 ; i <= len ; i++)
+		for (i = len / 2 ; i < len ; i++)
 			_putchar(str[i]);
 	else if (len % 2 != 0)
-		for (j = (len - 1) / 2 ; j <= len ; j++)
+		for (j = (len - 1) / 2 ; j < len ; j++)
 			_putchar(str[j]);
 	_putchar('\n');
 }
