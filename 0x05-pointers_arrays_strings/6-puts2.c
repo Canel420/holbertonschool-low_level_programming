4abcc92f2d2cf6ffb6d887c57efda696292ee405
Canel420 <4521@holbertonschool.com>
2022-02-21 19:35:18 -0500

    Printing even strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,11 +12,14 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i, len = 0;
+
+	while (str[len] != '\0')
 	{
-		if (*str % 2 == 0)
-			_putchar(*str);
-		str++;
+		len++;
 	}
+	for (i = 0 ; i < len - 1 ; i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
