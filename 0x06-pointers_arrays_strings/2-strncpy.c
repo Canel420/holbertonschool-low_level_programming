a852291c714aecc4fdfe7ed97a3fee494eb7dece
Canel420 <4521@holbertonschool.com>
2022-02-24 10:34:21 -0500

    Copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strncpy - Function that copies a string.
@@ -20,14 +19,14 @@ char *_strncpy(char *dest, char *src, int n)
 	i = 0;
 	while (i < n && src[i] != '\0')
 	{
-	        dest[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
 
 	i = i;
 	while (i < n)
 	{
-	        dest[i] = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
