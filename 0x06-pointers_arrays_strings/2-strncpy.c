ad26d046d515fafdf3e2a61ce5110c251b5a1e95
Canel420 <4521@holbertonschool.com>
2022-02-24 10:31:54 -0500

    Copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,33 +15,20 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, len = 1;
-	char *dest2 = dest, *resrc = src;
+	int i;
 
-	/* Remember to check if dest has space to save stuffs */
-
-	if (dest == NULL || src == NULL)
-		return (NULL);
-
-	/* length of string, initiated in 1 to account '\0' space */
-	while (*src != '\0')
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
-		len++;
-		src++;
+	        dest[i] = src[i];
+		i++;
 	}
 
-	/* if n is longer than len, function will crash */
-	if (n > len)
-		n = len;
-
-	/* src is at the end, need to resets it */
-
-	src = resrc;
-
-	/* copy src inside dest, taking in account n characters */
-
-	for (i = 0 ; i < n ; i++)
-		*dest++ = *src++;
-
-	return (dest2);
+	i = i;
+	while (i < n)
+	{
+	        dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
