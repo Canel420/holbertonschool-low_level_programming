b8c4a71f33e48325b9d454c267d5a553d0aa6f16
Canel420 <4521@holbertonschool.com>
2022-02-28 18:39:54 -0500

    String search 2

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
+/**
+ * _strpbrk - Function that searches a string for any of a set of bytes.
+ *
+ * @s: Pointer to string.
+ * @accept: Bytes to match.
+ *
+ * Description: Match first characters and on, from accept into s
+ *
+ * Return: Return character and rest of the string.
+ *
+ */
+
 char *_strpbrk(char *s, char *accept)
 {
 	char *orig = accept;
 
-        while (*s)
+	while (*s)
 	{
-	        while (*accept)
+		while (*accept)
 		{
 			if (*s == *accept)
 				return (s);
