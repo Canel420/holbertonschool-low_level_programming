0ac0c882bd462b478c1ca979661a87d5ab7605c2
Canel420 <4521@holbertonschool.com>
2022-03-07 13:04:54 -0500

    Fixed malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,10 +18,13 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	if (size == 0)
-		return ('\0');
-
 	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	if (size == 0)
+		return (NULL);
 
 	for (i = 0 ; i < size ; i++)
 		ptr[i] = c;
