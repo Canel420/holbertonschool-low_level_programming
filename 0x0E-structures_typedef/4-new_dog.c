7eb1377119063dbafba61c2b5e44022cc21f9150
Canel420 <4521@holbertonschool.com>
2022-03-14 20:32:42 -0500

    New dog with a copy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,42 +1,71 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
- * nd - new dog
- * @name: name's dog
- * @age: age's dog
- * @owner: owner's dog
+ * len - Measures the length of a string.
  *
- * Return: a pointer with the new structure
+ * @s: Pointer to string to measure.
+ *
+ * Description: Recursive count of characters in a
+ * string.
+ *
+ * Return: Number of characters.
  */
+
+int len(char *s)
+{
+	if (!s || !*s)
+		return (0);
+	return (1 + len(s + 1));
+}
+
+/**
+ * new_dog - Assign a new dog element.
+ *
+ * @name: Pointer to name of the dog.
+ * @age: Age of the dof in doggy years.
+ * @owner: Pointer to careteaker of the dog.
+ *
+ * Description: Creates a new dog instance.
+ *
+ * Return: Pointer to new dog.
+ *
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	/* dog_t is a user (me) created data type */
 	dog_t *nd;
-	int i, new_name, new_owner;
+	int n_len, o_len, i, j;
+	char *n2, *o2;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	/* Measure length of name and owner for copies */
+	n_len = len(name) + 1;
+	o_len = len(owner) + 1;
 
 	nd = malloc(sizeof(dog_t));
-	if (ndnew_dog == NULL || !name || !owner)
-	{
+	if (nd == NULL)
 		return (NULL);
-	}
-
-	for (new_name = 0 ; name[new_name] ; new_name++)
-		;
-	for (new_owner = 0 ; owner[new_owner] ; new_owner++)
-		;
-	nd->name = malloc(new_name + 1);
-	nd->owner = malloc(new_owner + 1);
-
-	if (!nd->name || !nd->owner)
-	{
+	/* Lets make the copies */
+	n2 = malloc(sizeof(char) * n_len);
+	if (n2 == NULL)
 		return (NULL);
-	}
-	for (i = 0 ; i < new_name ; i++)
-		nd->name[i] = name[i];
-	nd->name[i] = '\0';
-	nd->age = age;
+	for (i = 0 ; name[i] ; i++)
+		n2[i] = name[i];
+	n2[i] = '\0';
 
-	for (i = 0 ; i < new_owner ; i++)
-		nd->owner[i] = owner[i];
-	nd->owner[i] = '\0';
+	o2 = malloc(sizeof(char) * o_len);
+	if (o2 == NULL)
+		return (NULL);
+	for (j = 0 ; owner[j] ; j++)
+		o2[j] = owner[j];
+	o2[i] = '\0';
+
+	/* Assign the copied names into new dog */
+	(*nd).name = n2;
+	(*nd).age = age;
+	(*nd).owner = o2;
 	return (nd);
 }
