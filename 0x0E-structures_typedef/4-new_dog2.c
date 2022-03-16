92ec5fb4e0b226cbaac56669ae6430dcd5802f6e
Canel420 <4521@holbertonschool.com>
2022-03-15 20:00:00 -0500

    I dunno what the fck do with this

--- 0x0E-structures_typedef/4-new_dog2.c
+++ 0x0E-structures_typedef/4-new_dog2.c
@@ -1,7 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 /**
  * len - Measures the length of a string.
@@ -46,7 +45,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/* Measure length of name and owner for copies */
 	n_len = len(name) + 1;
 	o_len = len(owner) + 1;
-	printf("%d, %d ", n_len, o_len);
+	printf("%d, %d ", n_len, o_len);/* For debbugging */
 
 	nd = malloc(sizeof(dog_t));
 	if (nd == NULL)
@@ -58,7 +57,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0 ; name[i] ; i++)
 		n2[i] = name[i];
 	n2[i] = '\0';
-	printf("%s ", n2);
+	printf("%s ", n2); /* For debbugging */
 
 	o2 = malloc(sizeof(char) * o_len);
 	if (o2 == NULL)
@@ -66,7 +65,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (j = 0 ; owner[j] ; j++)
 		o2[j] = owner[j];
 	o2[i] = '\0';
-	printf("%s ", o2);
+	printf("%s ", o2); /* For debbugging */
 
 	/* Assign the copied names into new dog */
 	(*nd).name = n2;
