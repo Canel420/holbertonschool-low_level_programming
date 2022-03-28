e608cccbcd929a9dab5ad1c0e61086553de42912
Canel420 <4521@holbertonschool.com>
2022-03-28 18:40:36 -0500

    Print a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - Function that prints all the elements of a list_t list.
+ *
+ * @h: List to print
+ *
+ * Description: Take the elements of a given list of structure list_t
+ * and print them.
+ *
+ * Return: Amount of elements in list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int i;
+
+	for (i = 0; h ; i++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,10 @@
+# 0x12-singly_linked_lists
+
+This folder contains my first usage of linked lists.
+
+# Linked lists
+
+Like arrays, Linked List is a linear data structure. Unlike arrays, linked list
+elements are not stored at a contiguous location; the elements are linked using pointers.
+
+![alt text](https://media.geeksforgeeks.org/wp-content/cdn-uploads/gq/2013/03/Linkedlist.png)
