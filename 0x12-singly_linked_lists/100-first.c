9eb8a8ba04e98e583767ea002dc990041723af2b
Canel420 <4521@holbertonschool.com>
2022-03-29 18:22:35 -0500

    constructor usage

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+#include <stdio.h>
 
 void __attribute__((constructor)) begin(void);
 /**
@@ -8,6 +8,6 @@ void __attribute__((constructor)) begin(void);
 
 void begin(void)
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
