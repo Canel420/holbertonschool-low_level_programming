a39916d7a42d0755416c3d786d85b3ac63612569
Canel420 <4521@holbertonschool.com>
2022-03-31 14:07:44 -0500

    Add integer node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * add_nodeint - Function that adds a new node at the beginning of a listint_t
- * list.
+ * add_nodeint - Function that adds a new node at the beginning of a
+ * listint_t list.
  *
  * @head: Head node of the list
  * @n: Integer data for the new node.
  *
- * Description: Takes a linked list head node and add a new node with n content.
+ * Description: Takes a linked list head node and add a new node with n
+ * content.
  *
  * Return: Address to the new node.
  *
@@ -16,6 +17,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
