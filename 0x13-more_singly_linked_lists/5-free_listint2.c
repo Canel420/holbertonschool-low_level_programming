063c3ebe25a8b6010cbdfed72c9192ff830b52e8
Canel420 <4521@holbertonschool.com>
2022-03-31 16:01:06 -0500

    Free lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function that frees a listint_t list.
+ *
+ * @head: Reference to reference of the head node of list.
+ *
+ * Description: Take a list with double pointer reference to head
+ * and deletes it.
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	/* Deref head to get the real head */
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	/* Deref head to affect the real head back in the caller */
+	*head = NULL;
+}
