e0d1edb5029e3181c2d2ec1543250556fd386748
Canel420 <4521@holbertonschool.com>
2022-04-01 13:11:39 -0500

    Get a node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,9 +20,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	for (i = 0; head ; i++)
+	while (head)
+	{
 		if (i == index)
 			return (head);
-	head = head->next;
+		head = head->next;
+		i++;
+	}
 	return (NULL);
 }
