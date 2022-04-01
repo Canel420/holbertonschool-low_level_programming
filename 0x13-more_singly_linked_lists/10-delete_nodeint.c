a7220d0b16fa25241f79ecfb285b37c72a27fbb2
Canel420 <4521@holbertonschool.com>
2022-04-01 17:08:18 -0500

    add NULL case

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *del;
 	unsigned int i = 1;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 
 	temp = *head;
