3ee7e206a2f2607c16375afd7af0bcd3eec7688c
Canel420 <4521@holbertonschool.com>
2022-04-01 17:28:58 -0500

    find loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -14,6 +14,9 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow_p = head, *fast_p = head;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (slow_p && fast_p && fast_p->next)
 	{
 		slow_p = slow_p->next;
