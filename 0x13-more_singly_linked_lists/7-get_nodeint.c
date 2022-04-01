ce7a7c7684c7ae861f1451192bce528b0f29d0b6
Canel420 <4521@holbertonschool.com>
2022-04-01 16:43:52 -0500

    Integer set to zero

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,14 +15,17 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
 
-	for (i = 0; head; i++)
+	while (head)
+	{
 		if (i == index)
 			return (head);
-	head = head->next;
+		head = head->next;
+		i++;
+	}
 	return (NULL);
 }
