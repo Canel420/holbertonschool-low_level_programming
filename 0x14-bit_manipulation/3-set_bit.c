2707f78d409c757ed5c3e5bbc5939abed07eb83f
Canel420 <4521@holbertonschool.com>
2022-04-04 20:04:26 -0500

    bety fixed

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -15,7 +15,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int get = 1;
 
-	if (sizeof(unsigned long int)*8) - 1 < index)
+	if ((sizeof(unsigned long int) * 8) - 1 < index)
 	{
 		return (-1);
 	}
