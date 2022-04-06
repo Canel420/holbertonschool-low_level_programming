008aad2e43f96ab79e4ea153fc3f295a1b8b3ef9
Canel420 <4521@holbertonschool.com>
2022-04-06 17:38:12 -0500

    fuck

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * close_error - Functions that prints error message on closing.
+ *
+ * @fd: File descriptor to file for closing.
+ *
+ * Description: Take the file descriptor of a file and closes it.
+ * If it fails prints a message.
+ *
+ * Return: 1 on success, -1 if it fails.
+ */
+
 int close_error(int fd)
 {
 	int error;
@@ -10,6 +21,16 @@ int close_error(int fd)
 	return (error);
 }
 
+/**
+ * main - Program to copy files
+ *
+ * @argc: Argument count.
+ * @argv: Reference to array of arguments.
+ *
+ * Return: 0 on success.
+ *
+ */
+
 int main(int argc, char *argv[])
 {
 	char *buff[1024];
@@ -18,24 +39,18 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+		exit(97); }
 	in_fd = open(argv[1], O_RDONLY);
 	if (in_fd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
+		exit(98); }
 	out_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (out_fd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-        while (copy)
+		exit(99); }
+	while (copy)
 	{
 		copy = read(in_fd, buff, 1024);
 		if (copy < 0)
@@ -43,28 +58,21 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			close(in_fd);
 			close(out_fd);
-			exit(98);
-		}
-
+			exit(98); }
 		paste = write(out_fd, buff, copy);
 		if (paste < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(in_fd);
 			close(out_fd);
-			exit(99);
-		}
-
-	}
+			exit(99); } }
 	error = close_error(out_fd);
 	if (error < 0)
 	{
 		close_error(in_fd);
-		exit(100);
-	}
+		exit(100); }
 	error = close_error(in_fd);
 	if (error < 0)
 		exit(100);
-
 	return (0);
 }
