9590f154629e55695dd0a3820297a114b373005a
Canel420 <4521@holbertonschool.com>
2022-04-05 19:36:27 -0500

    Creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - Measures a string.
+ *
+ * @s: String to be measured.
+ *
+ * Description: Count the number of characters in string.
+ *
+ * Return: Count.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s; i++)
+		;
+	return (i);
+}
+
+/**
+ * create_file - Function that creates a file.
+ *
+ * @filename: Name for the new file.
+ * @text_content: Content to write in file.
+ *
+ * Description: Creates a file with permission read-write owner and writes
+ * on it.If file already exist erase its content.
+ *
+ * Return: 1 on success, -1 if it fails.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int new, len, f_write;
+
+	if (filename == NULL)
+		return (-1);
+
+	len = _strlen(text_content);
+	new = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (new < 0)
+		return (-1);
+
+	f_write = write(new, text_content, len);
+	close(new);
+
+	if (f_write < 0)
+		return (-1);
+	return (1);
+}
