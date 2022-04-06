917130be00647c12349b1a2cb99ddbf7bce4c3af
Canel420 <4521@holbertonschool.com>
2022-04-05 19:57:47 -0500

    reorder POXIS

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	len = _strlen(text_content);
-	new = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	new = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (new < 0)
 		return (-1);
 
