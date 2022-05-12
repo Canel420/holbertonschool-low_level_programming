699b3efbe9587c6303f27a9b55f47535617c9217
Canel420 <4521@holbertonschool.com>
2022-05-12 11:42:55 -0500

    insert node at position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/* Iterate until the index previous node */
 	if (idx != 0)
 	{
-		for (i = 0; i < idx - 1 && current != NULL; i++)
+		for (i = 0; i < idx - 1 && temp != NULL; i++)
 			temp = temp->next;
 		if (temp == NULL)
 			return (NULL);
