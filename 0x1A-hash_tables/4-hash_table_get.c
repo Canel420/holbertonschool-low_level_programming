e6bf6570e863336c91dc033b98847f9479abb31d
Canel420 <4521@holbertonschool.com>
2022-06-06 10:59:37 -0500

    change redirection

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,10 +14,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp = NULL;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-		key == NULL || strlen(key) == 0)
-			return (NULL);
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
 
-	index = key_index((unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 
 	if (tmp != NULL)
