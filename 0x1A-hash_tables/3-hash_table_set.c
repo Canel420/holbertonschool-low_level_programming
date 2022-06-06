16978c1eaa9262cd9a492c98f1f170750bff86ae
Canel420 <4521@holbertonschool.com>
2022-06-05 19:07:12 -0500

    new node

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    hash_node_t *new = NULL, *tmp = NULL;
+    unsigned long int index = 0;
+    char *new_value = NULL;
+
+    if (!ht || !key || !value || strlen(key) == 0 || ht->size == 0)
+        return (0);
+
+    new = malloc(sizeof(hash_node_t));
+    if (!new)
+        return (0);
+
+    strcpy(new->key, key);
+    strcpy(new->value, value);
+    new->next = NULL;
+    
+    index = key_index((const unsigned char *)key, ht->size);
+    tmp = ht->array[index];
+    while (tmp != NULL)
+    {
+        if (strcmp(tmp->key, key) == 0)
+        {
+            new_value = strdup(value);
+            if (!new_value)
+                return (0);
+            free(tmp->value);
+            tmp->value = new_value;
+            return (1);
+        }
+        tmp = tmp->next;
+    }
+    new->next = ht->array[index];
+    ht->array[index] = new;
+    return (1);
+}
