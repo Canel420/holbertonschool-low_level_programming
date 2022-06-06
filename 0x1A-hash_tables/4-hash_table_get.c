adfbf83131d7917b2ae5968a82d6f73ce49b0848
Canel420 <4521@holbertonschool.com>
2022-06-06 11:06:43 -0500

    change conditional

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,13 +17,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	    key == NULL || strlen(key) == 0)
 		return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 
-	if (tmp != NULL)
+	while (tmp != NULL)
 	{
 		if (strcmp(tmp->key, key) == 0)
 			return (tmp->value);
+		tmp = tmp->next;
 	}
 	return (NULL);
 }
