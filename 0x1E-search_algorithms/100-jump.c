c57d4b56caf7afc2384cf1207ee5dec8a5627aad
Canel420 <4521@holbertonstudents.com>
2022-08-10 09:57:17 -0500

    null cases

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,6 +17,9 @@ int jump_search(int *array, size_t size, int value)
 	size_t start = 0, i = 0;
 	size_t end = 0;
 
+	if (!array || size <= 0)
+		return (-1);
+
 	while (array[end] < value && end < size)
 	{
 		start = end;
