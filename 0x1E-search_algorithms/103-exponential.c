eb733f9f876d6b40a3bd0d71bc328db8a3e84f7b
Canel420 <4521@holbertonstudents.com>
2022-08-11 11:23:23 -0500

    exponential

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -38,33 +38,35 @@ void print_search(int *array, size_t start, size_t end)
 }
 
 /**
- * recursive_binary - search a given value in a sorted array.
+ * binary_search - Searches for a value in a sorted array.
  *
  * @array: pointer to the first element of the array.
- * @start: lowest index in array.
- * @end: highest index in array.
+ * @size: number of elements in array.
  * @value: value to search for.
  *
- * Return:  the index where the value is at or -1 if couldn't be found.
+ * Description: search for a given value using binary search
+ * algorithm.
+ * Return: the index where the value is at or -1 if couldn't be found.
  */
-
-int recursive_binary(int *array, size_t start, size_t end, int value)
+int binary_search(int *array, size_t size, int value)
 {
-	size_t mid = 0;
+	size_t low = 0, high = size - 1, mid = 0;
 
-	if (start <= end)
+	if (array == NULL || size <= 0)
+		return (-1);
+
+	while (low <= high)
 	{
-		mid = (start + (end - start) / 2);
-		if (array[mid] == value)
-		{
-			print_search(array, start, end - 1);
-			return (mid);
-		}
-		if (array[mid] > value)
-			return (recursive_binary(array, start, mid - 1, value));
+		print_search(array, low, high);
 
-		print_search(array, start, end - 1);
-		return (recursive_binary(array, mid + 1, end, value));
+		mid = low + (high - low) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
 	}
 	return (-1);
 }
@@ -80,19 +82,22 @@ int recursive_binary(int *array, size_t start, size_t end, int value)
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t start = 0, end = size - 1, i = 1;
+	size_t i = 1, low = 0;
+	int r;
 
-	if (end - start <= 0 || !array)
-		return (-1);
-
-	while (i < (end - start))
+	if (array != NULL && size > 0)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] < value)
+		while (i < size && array[i] < value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 			i *= 2;
-		else
-			break;
+		}
+		low = i / 2;
+		i = min(size - 1, i);
+		printf("Value found between indexes [%lu] and [%lu]\n", low, i);
+		r = binary_search(array + low, i + 1 - low, value);
+		if (r >= 0)
+			return (r + low);
 	}
-	printf("Value found between indexes [%lu] and [%lu]\n", i / 2, min(i, end));
-	return (recursive_binary(array, i / 2, i, value));
+	return (-1);
 }
